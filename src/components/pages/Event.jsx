1a3978d0f6ed09bb78ddf853afc3e6572f89a61c
Dino Marchiori <dinaco1@gmail.com>
2022-06-06 14:02:23 +0100

    included navbar event page

--- src/components/pages/Event.jsx
+++ src/components/pages/Event.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import EventInfo from "../Event/EventInfo";
+import Navbar from "../Navbar";
 
 function Event() {
   return (
     <div>
+      <Navbar />
       <EventInfo />
     </div>
   );
