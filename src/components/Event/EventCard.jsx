f652bc074531238b48558ff9f900cb0d4a4fe09a
Dino Marchiori <dinaco1@gmail.com>
2022-06-09 21:04:20 +0100

    fixing order listing only when attending event

--- src/components/Event/EventCard.jsx
+++ src/components/Event/EventCard.jsx
@@ -46,71 +46,79 @@ function EventCard({ eventInfo, user }) {
 
   return (
     <Card sx={{ my: 2 }}>
-      <Link to={`/event/${eventInfo._id}`}>
-        <CardMedia
-          component='img'
-          image={eventInfo.splashImg}
-          alt={eventInfo.name}
-        />
-      </Link>
-      <Box sx={{ p: 2 }}>
-        <Stack
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-          spacing={0.5}>
+      {!eventInfo && <h2>No events found</h2>}
+      {eventInfo && (
+        <>
           <Link to={`/event/${eventInfo._id}`}>
-            <Typography variant='h5' fontWeight={700}>
-              {eventInfo.name}
-            </Typography>
+            <CardMedia
+              component='img'
+              image={eventInfo.splashImg}
+              alt={eventInfo.name}
+            />
           </Link>
-          {eventInfo.customers[0] && eventInfo.customers[0].profileImg && (
-            <AvatarGroup spacing='small' total={eventInfo.customers.length}>
-              {eventInfo.customers.slice(0, 2).map((customer) => (
-                <Avatar
-                  key={customer._id}
-                  alt={customer.name}
-                  src={customer.profileImg}
-                />
-              ))}
-            </AvatarGroup>
-          )}
-        </Stack>
-        {user && eventInfo && <EventActions user={user} event={eventInfo} />}
-      </Box>
-      <Divider />
-      <Stack
-        direction='row'
-        alignItems='center'
-        justifyContent='space-between'
-        sx={{ px: 2, py: 1, bgcolor: "background.default" }}>
-        <Chip
-          icon={<CalendarMonthIcon />}
-          label={moment(eventInfo.date).format("DD/MM/YYYY")}
-        />
-        <StyledLocation variant='body2' color='text.secondary'>
-          <LocationOnIcon sx={{ color: grey[500] }} /> {eventInfo.location.city}
-          , {eventInfo.location.state} - {eventInfo.location.country}
-        </StyledLocation>
-      </Stack>
-      <Divider />
-      <CardActions onClick={handleExpandClick} disableSpacing>
-        <Typography variant='body1'>Event Details</Typography>
-        <ExpandMore
-          expand={expanded}
-          aria-expanded={expanded}
-          aria-label='show more'>
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout='auto' unmountOnExit>
-        <CardContent>
-          <Typography paragraph>{eventInfo.description}</Typography>
-        </CardContent>
-      </Collapse>
+          <Box sx={{ p: 2 }}>
+            <Stack
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+              spacing={0.5}>
+              <Link to={`/event/${eventInfo._id}`}>
+                <Typography variant='h5' fontWeight={700}>
+                  {eventInfo.name}
+                </Typography>
+              </Link>
+              {eventInfo.customers[0] && eventInfo.customers[0].profileImg && (
+                <AvatarGroup spacing='small' total={eventInfo.customers.length}>
+                  {eventInfo.customers.slice(0, 2).map((customer) => (
+                    <Avatar
+                      key={customer._id}
+                      alt={customer.name}
+                      src={customer.profileImg}
+                    />
+                  ))}
+                </AvatarGroup>
+              )}
+            </Stack>
+            {user && eventInfo && (
+              <EventActions user={user} event={eventInfo} />
+            )}
+          </Box>
+          <Divider />
+          <Stack
+            direction='row'
+            alignItems='center'
+            justifyContent='space-between'
+            sx={{ px: 2, py: 1, bgcolor: "background.default" }}>
+            <Chip
+              icon={<CalendarMonthIcon />}
+              label={moment(eventInfo.date).format("DD/MM/YYYY")}
+            />
+            <StyledLocation variant='body2' color='text.secondary'>
+              <LocationOnIcon sx={{ color: grey[500] }} />{" "}
+              {eventInfo.location.city}, {eventInfo.location.state} -{" "}
+              {eventInfo.location.country}
+            </StyledLocation>
+          </Stack>
+          <Divider />
+          <CardActions onClick={handleExpandClick} disableSpacing>
+            <Typography variant='body1'>Event Details</Typography>
+            <ExpandMore
+              expand={expanded}
+              aria-expanded={expanded}
+              aria-label='show more'>
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout='auto' unmountOnExit>
+            <CardContent>
+              <Typography paragraph>{eventInfo.description}</Typography>
+            </CardContent>
+          </Collapse>
+        </>
+      )}
     </Card>
   );
 }
